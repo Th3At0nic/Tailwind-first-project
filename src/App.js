a3e6159c8e5b0b2ba0da8efb91b43b885b3c27ec
Th3At0nic <islammdrahatul@gmail.com>
2021-06-14 22:42:52 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import breakfastImg from "../src/images/food.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div
+      className="min-h-screen w-screen flex justify-center items-center flex-col-reverse lg:flex-row"
+      style={{
+        background: "linear-gradient(to bottom, #F0F4FD, #A1A3BA)",
+      }}
+    >
+      <div className="mt-5 text-center">
+        <h1 className="font-black text-5xl">
+          Fresh Healthy <br />
+          <span className="font-quicksand font-light">
+            Delicious Salads.
+          </span>{" "}
+        </h1>
+        <p className="text-gray-700  text-base">
+          We make fresh and healthy foods.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <span className="inline-block bg-indigo-500 px-10 py-5 rounded-full shadow-lg uppercase tracking-wide mt-5">
+          Explore
+        </span>
+      </div>
+      <img className="w-screen lg:max-w-xl" src={breakfastImg} alt="food" />
     </div>
   );
 }

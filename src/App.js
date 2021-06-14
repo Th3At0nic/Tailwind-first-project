3bc644da0a3e3c7723052409c0428820cbb4893e
Th3At0nic <islammdrahatul@gmail.com>
2021-06-14 23:12:36 +0600

    final commit , all code clean

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import "./App.css";
 import breakfastImg from "../src/images/food.svg";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [isAnimated, setIsAnimated] = useState(false);
+
+  useEffect(() => {
+    setIsAnimated(true);
+  }, []);
   return (
     <div
-      className="min-h-screen w-screen flex justify-center items-center flex-col-reverse lg:flex-row"
+      className="min-h-screen w-screen flex justify-center items-center flex-col-reverse lg:flex-row lg:justify-around"
       style={{
         background: "linear-gradient(to bottom, #F0F4FD, #A1A3BA)",
       }}
     >
-      <div className="mt-5 text-center">
-        <h1 className="font-black text-5xl">
+      <div
+        className={`${
+          isAnimated ? "scale-y-100" : "scale-y-0"
+        } mt-5 text-center lg:text-left transform transition duration-1000 ease-in-out`}
+      >
+        <h1 className="font-black text-5xl lg:text-6xl">
           Fresh Healthy <br />
           <span className="font-quicksand font-light">
             Delicious Salads.
@@ -23,7 +33,13 @@ function App() {
           Explore
         </span>
       </div>
-      <img className="w-screen lg:max-w-xl" src={breakfastImg} alt="food" />
+      <img
+        className={`${
+          isAnimated ? "scale-150" : "translate-x-full translate-y-full"
+        }w-screen lg:max-w-xl  transform transition duration-1000 ease-in-out`}
+        src={breakfastImg}
+        alt="food"
+      />
     </div>
   );
 }
